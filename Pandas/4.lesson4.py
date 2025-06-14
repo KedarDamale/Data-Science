dea2b2e3c90f53acf192e1471bbdc9d08f3c1745
KedarDamale <damalekedar@gmail.com>
2025-06-14 18:36:33 +0530

    Added pandas notes

--- Pandas/4.lesson4.py
+++ Pandas/4.lesson4.py
@@ -20,3 +20,41 @@ data={
     "EHF_IA1":[11,10,10,14,13,10,11,9,14,18],
     "EHF_IA2":[15,16,16,16,16,14,14,18,17,18]
 }
+df=pd.DataFrame(data)
+#creating a column of our own lets say average of two marks of DMBVI subject
+
+# df['Average_DMBI']=((df["DMBI_IA1"]+df["DMBI_IA2"])/2)
+
+
+# #WE CAN USE DIFFERNT FUNCTION SUCH AS FLOOR OR CEIL TO ROUND TOO
+# import numpy as np
+# df['Average_DMBI']=np.ceil(((df["DMBI_IA1"]+df["DMBI_IA2"])/2))
+
+#print(df.tail(5))
+
+
+#inserting data using insert() method
+
+# df.insert(location,"column name",data)
+
+# df.insert(0,"StudentID",[1,2,3,4,5,6,7,8,9,10])
+
+# print(df.tail(5))
+
+"""
+   StudentID                    Names  DMBI_IA1  ...  EHF_IA1  EHF_IA2  Average_DMBI
+5          6      BHATKAR SAHIL VILAS         6  ...       10       14           9.0
+6          7         BHOMBAL ZIA AMIR         8  ...       11       14          11.0
+7          8   BHUJBAL YUKTA SADASHIV        10  ...        9       18           9.0
+8          9  DABHOLKAR PRACHI PRADIP        18  ...       14       17          16.0
+9         10     DAMALE KEDAR PRAVIN         17  ...       18       18          17.0
+
+"""
+#insertion using assignment wont have a control where the column will be inserted but in insert method we have that control
+
+import numpy as np
+df.insert(4,"DMBI-Average",np.ceil((df['DMBI_IA1']+df["DMBI_IA2"])/2))
+
+print(df[df['DMBI-Average'] > 16]['Names'].to_string(index=False).strip().split(" ")[1])
+
+
