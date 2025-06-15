f25d298e422db1c6252f3d570a6955ceba8a0fa3
KedarDamale <damalekedar@gmail.com>
2025-06-16 01:15:46 +0530

    Added some pandas notes

--- Pandas/13.lesson13.py
+++ Pandas/13.lesson13.py
@@ -1,3 +1,5 @@
 # print(df.duplicated()) Returns true or false for every row if it has duplicate in the frame
 
 #to remove duplicates: df.drop_duplicates(inplace=True)
+
+# df['colum name']value_count()---> index and values
